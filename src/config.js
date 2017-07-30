686e743a5dac2e6cf545419ce3b8fa71dbee2646
caiklaus <caiklaus@gmail.com>
2017-07-30 18:19:59 +0800

    Add API

--- src/config.js
+++ src/config.js
@@ -11,5 +11,10 @@
     //listen server
     set('web.port', 3000);
 
+    // session redis connection
+    set('web.session.redis', {
+      host: '127.0.0.1',
+      port: 6379,
+    });
 
  };
--- src/init/express.js
+++ src/init/express.js
@@ -9,12 +9,38 @@ import express from 'express';
 import serveStatic from 'serve-static';
 import bodyParser from 'body-parser';
 import multiparty from 'multiparty';
+import session from 'express-session';
+import _RedisStore from 'connect-redis';
+const RedisStore = _RedisStore(session);
 
 module.exports = function (done) {
+
+  const debug = $.createDebug('init:express');
+  debug('initing Express...');
 
   const app = express();
 
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended:false }));
-  
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(multipart());
+  app.use(session({
+    secret: $.config.get('web.session.secret'),
+    store: new RedisStore($.config.get('web.session.redis')),
+  }));
+
+  const router = express.Router();
+
+const routerWrap = {};
+['get', 'head', 'post', 'put', 'del', 'delete'].forEach(method => {
+  routerWrap[method] = function (path, ...fnList) {
+    fnList = fnList.map(fn => {
+      return function (req, res, next) {
+        const ret = fn(req, res, next);
+        if (ret && ret.catch) ret.catch(next);
+      };
+    });
+    router[method](path, ...fnList);
+  };
+});
+$.router = routerWrap;
 };
